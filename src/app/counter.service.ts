c5f047146538d3698df8f40b1434b1c80f025d7c
Sufyan Abdul Ghani <40076204+SUFIAG@users.noreply.github.com>
2023-10-26 02:09:29 +0500

    Initial commit

---
+++ src/app/counter.service.ts
@@ -0,0 +1,13 @@
+export class CounterService {
+  activeToInactiveCounter = 0;
+  inactiveToActiveCounter = 0;
+
+  incrementActiveToInactive () {
+    this.activeToInactiveCounter++;
+    console.log('Active to Inactive: ' + this.activeToInactiveCounter);
+  }
+  incrementInactiveToActive () {
+    this.inactiveToActiveCounter++;
+    console.log('Inactive to Active: ' +this.inactiveToActiveCounter);
+  }
+}
